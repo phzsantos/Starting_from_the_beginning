0664d4b45c8386f453c8a1ec376ebfd29e026160
phzsantos <phzsantos2002@gmail.com>
2021-10-18 20:08:33 -0300

    Saida de dados

---
+++ 01 - C/00 - Saida de dados.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Primeiro exemplo: \n");
+
+    printf("Bom dia!");
+    printf("Boa noite!\n");
+
+
+    printf("Segundo exemplo: \n");
+
+    int x, y;
+
+    x = 10;
+    y = 20;
+    printf("%d\n",x);
+    printf("%d\n",y);
+
+
+    printf("Terceiro exemplo: \n");
+
+    double x2;
+
+    x2 = 2.3456;
+    printf("%.2lf\n", x2);
+
+
+    printf("Quarto exemplo: \n");
+
+    int idade;
+    double salario;
+    char nome[50];
+    char sexo;
+
+    idade = 32;
+    salario = 4560.9;
+    strcpy(nome, "Maria Silva");
+    sexo = 'F';
+
+    printf("A funcionaria %s, sexo %c, ganha %.2lf e tem %d anos", nome, sexo, salario, idade);
+
+    return 0;
+}
