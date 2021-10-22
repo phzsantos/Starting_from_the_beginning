e698eaad87a95e3895c7b489eea3d66a3f7f063d
phzsantos <phzsantos2002@gmail.com>
2021-10-21 21:12:32 -0300

    Entrada de dados complexa fgets e strtok

--- 01 - C/00 - Entrada de dados.c
+++ 01 - C/00 - Entrada de dados.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+
+void limpar_entrada()
+{
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+void ler_texto(char *buffer, int length)
+{
+    fgets(buffer, length, stdin);
+    strtok(buffer, "\n");
+}
 
 int main()
 {
+    double salario1, salario2;
+    char nome1[50], nome2[50];
     int idade;
-    double salario, altura;
-    char genero;
-    char nome[50];
+    char sexo;
 
-    printf("Digite o valor da idade: ");
+    printf("Nome da primeira pessoa: ");
+    ler_texto(nome1, 50);
+
+    printf("Salario da primeira pessoa: ");
+    scanf("%lf", &salario1);
+
+    printf("Nome da segunda pessoa: ");
+    limpar_entrada();
+    ler_texto(nome2, 50);
+
+    printf("Salario da segunda pessoa: ");
+    scanf("%lf", &salario2);
+
+    printf("Digite uma idade: ");
     scanf("%d", &idade);
-    printf("Digite o salario: ");
-    scanf("%lf", &salario);
-    printf("Digite a altura: ");
-    scanf("%lf", &altura);
-    printf("Digite o nome da pessoa: ");
-    scanf("%s", nome);
 
-    printf("IDADE: %d\n", idade);
-    printf("SALARIO: %.2lf\n", salario);
-    printf("ALTURA: %.2lf\n", altura);
-    printf("NOME: %s\n", nome);
+    printf("Digite um sexo (F/M): ");
+    limpar_entrada();
+    scanf("%c", &sexo);
+
+    printf("Nome 1: %s\n", nome1);
+    printf("Salario 1: %.2lf\n", salario1);
+    printf("Nome 2: %s\n", nome2);
+    printf("Salario 2: %.2lf\n", salario2);
+    printf("Idade: %d\n", idade);
+    printf("Sexo: %c\n", sexo);
 
     return 0;
 }
