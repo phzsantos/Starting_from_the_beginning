406806f4ce4f80bfa368cb3c9d42c81f03574986
phzsantos <phzsantos2002@gmail.com>
2021-11-08 19:53:30 -0300

    Processamento de dados e casting

---
+++ 02 - Cpp/00 - Processamento de dados e casting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+	double resultado;
+
+	x = 5;
+	y = 2;
+
+	resultado = (double)x / y;
+
+    cout << resultado << '\n';
+
+	return 0;
+}
