d33f08e6958af1de17248ee08fe25486a2220b64
phzsantos <phzsantos2002@gmail.com>
2021-11-13 20:41:33 -0300

    Exercicio 03 menor de tres

---
+++ 02 - Cpp/03 - Menor de tres.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int primeiro, segundo, terceiro, menor;
+
+    cout << "Primeiro valor: ";
+    cin >> primeiro;
+    cout << "Segundo valor: ";
+    cin >> segundo;
+    cout << "Terceiro valor: ";
+    cin >> terceiro;
+
+    if ((primeiro < segundo) && (primeiro < terceiro))
+    {
+        menor = primeiro;
+    }
+    else if (segundo < terceiro)
+    {
+        menor = segundo;
+    }
+    else
+    {
+        menor = terceiro;
+    }
+
+    cout << "MENOR = " << menor << '\n';
+
+    return 0;
+}
