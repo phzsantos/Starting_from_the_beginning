1f42e9d969bba6346f2d47332ecce3683d38783e
phzsantos <phzsantos2002@gmail.com>
2021-11-18 20:42:51 -0300

    Saida de dados Java

---
+++ 03 - Java/00 - Saida de dados.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{		
+		Locale.setDefault(Locale.US);
+		
+		int idade;
+		double salario;
+		String nome;
+		char sexo;
+		
+		idade = 32;
+		salario = 4560.9;
+		nome = "Maria Silva";
+		sexo = 'F';
+		
+		System.out.println("A funcionaria " + nome + ", sexo " + sexo + ", ganha " 
+		                   + String.format("%.2f", salario) + " e tem " + idade + " anos");
+	}
+}
