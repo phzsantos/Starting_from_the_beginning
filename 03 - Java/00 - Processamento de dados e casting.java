1b83667165c7dc541cf834b723e90a765b3da570
phzsantos <phzsantos2002@gmail.com>
2021-11-19 22:23:07 -0300

    Processamento de dados e casting

---
+++ 03 - Java/00 - Processamento de dados e casting.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{		
+		Locale.setDefault(Locale.US);
+		
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		resultado = (double) a / b;
+		System.out.println(resultado);
+		
+		double c;
+		int d;
+
+		c = 5.0;
+		d = (int) c;
+		System.out.println(d);	
+	}
+}
