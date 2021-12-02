ea91e0e0528fc2a818a88dd74091d781cda0fd03
phzsantos <phzsantos2002@gmail.com>
2021-12-01 22:11:17 -0300

    Tipos de dados e variaveis Csharp

---
+++ 04 - Csharp/00 - Tipos de dados e variaveis/00 - Tipos de dados e variaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            double salario, altura;
+            char genero;
+            string nome;
+
+            idade = 20;
+            salario = 5800.5;
+            altura = 1.63;
+            genero = 'F';
+            nome = "Maria Silva";
+
+            Console.WriteLine("IDADE: " + idade);
+            Console.WriteLine("SALARIO: " + salario);
+            Console.WriteLine("ALTURA: " + altura);
+            Console.WriteLine("GENERO: " + genero);
+            Console.WriteLine("NOME: " + nome);
+        }
+    }
+}
