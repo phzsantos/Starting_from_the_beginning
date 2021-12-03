7f078038a8adf56899a30db0d98676eb42a41473
phzsantos <phzsantos2002@gmail.com>
2021-12-03 20:14:03 -0300

    Processamento de dados e casting

---
+++ 04 - Csharp/00 - Processamento de dados e casting/00 - Processamento de dados e casting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int x, y;
+            double resultado;
+
+            x = 5;
+            y = 2;
+
+            resultado = (double) x / y;
+
+            Console.WriteLine(resultado.ToString("F2", CI));
+
+            double a;
+            int b;
+
+            a = 5.2;
+            b = (int) a;
+
+            Console.WriteLine(b.ToString("F2", CI));
+        }
+    }
+}
