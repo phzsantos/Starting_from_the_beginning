6f43a3015e7682fadf1c7572cb43b368c1406d2d
phzsantos <phzsantos2002@gmail.com>
2021-12-04 21:06:37 -0300

    Entrada de dados

---
+++ 04 - Csharp/00 - Entrada de dados/00 - Entrada de dados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double salario1, salario2;
+            string nome1, nome2;
+            int idade;
+            char sexo;
+
+            Console.Write("Digite o nome da primeira pessoa: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Digite o salario da primeira pessoa: ");
+            salario1 = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite o nome da segunda pessoa: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Digite o salario da segunda pessoa: ");
+            salario2 = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite uma idade: ");
+            idade = int.Parse(Console.ReadLine());
+            Console.Write("Digite um sexo (F/M): ");
+            sexo = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOME 1: " + nome1);
+            Console.WriteLine("SALARIO 1: " + salario1.ToString("F2", CI));
+            Console.WriteLine("NOME 2: " + nome2);
+            Console.WriteLine("SALARIO 2: " + salario2.ToString("F2", CI));
+            Console.WriteLine("IDADE: " + idade);
+            Console.WriteLine("SEXO: " + sexo);
+        }
+    }
+}
