9d7c19bbd436b9e1cb4f5e47c272a9c490c6373f
phzsantos <phzsantos2002@gmail.com>
2021-12-05 17:00:58 -0300

    Matrizes e vetores Csharp

---
+++ 04 - Csharp/00 - Matrizes/00 - Matrizes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, m, n;
+
+            Console.Write("Quantas linhas vai ter a matriz: ");
+            m = int.Parse(Console.ReadLine());
+            Console.Write("Quantas colunas vai ter a matriz: ");
+            n = int.Parse(Console.ReadLine());
+            
+            int[,] matriz = new int [m, n]; 
+
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    matriz[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("MATRIZ DIGITADA: ");
+            for (i = 0; i < m; i++)
+            {
+                for (j=0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04 - Csharp/00 - Vetores/00 - Vetores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int i, n;
+
+            Console.Write("Quantos numeros voce vai digitar: ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vetor = new double[n];
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vetor[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("NUMEROS DIGITADOS: ");
+            for (i = 0; i < n; i++)
+            { 
+                Console.WriteLine(vetor[i].ToString("F2", CI));
+            }
+        }
+    }
+}
